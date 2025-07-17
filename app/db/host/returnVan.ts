e23054203f428128ccff0efd4fdda42613a2e85f
Coinnich <>
2025-07-17 15:25:16 +1200

    realized that actually unique constraint previosly placed on Rent table was incorrect. renterId and rentedTo are not unique unless in combination with vanId as multiple vans may be returned the same day

--- app/db/host/returnVan.ts
+++ app/db/host/returnVan.ts
@@ -1,15 +1,5 @@
 import { prisma } from '~/lib/prisma';
 // import prisma from '~/lib/prisma';
-// export async function returnVan(rentId: string) {
-// 	return prisma.rent.update({
-// 		where: {
-// 			id: rentId,
-// 		},
-// 		data: {
-// 			rentedTo: new Date(),
-// 		},
-// 	});
-// }
 
 export async function returnVan(
 	rentId: string,
