a1c3fadf613f576d2af71fb5c95e838e8c838f6b
Coinnich <>
2025-07-22 22:17:41 +1200

    better auth prisma integration not working...?

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -18,8 +18,6 @@
 
 import { PrismaClient } from '@prisma/client';
 
-// import { withAccelerate } from '@prisma/extension-accelerate';
-// .$extends(withAccelerate());
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient();
