40c8ce12ab0288ef307dfed776ac2948ea46ba07
Coinnich <>
2025-07-26 18:41:50 +1200

    have made further changes to simplify layout of van detail

--- app/components/Van/VanDetail.tsx
+++ app/components/Van/VanDetail.tsx
@@ -3,7 +3,7 @@ import { href } from 'react-router';
 import Image from '~/components/Image';
 import { Badge, badgeVariants } from '~/components/ui/badge';
 import { VAN_DETAIL_IMG_SIZES } from '~/constants/constants';
-import { createSrcSet } from '~/utils/createSrcSet';
+import { createResponsiveSrcSet } from '~/utils/createSrcSet';
 import { displayPrice } from '~/utils/displayPrice';
 import { cn } from '~/utils/utils';
 import CustomLink from '../CustomLink';
@@ -25,40 +25,72 @@ export default function VanDetail({
 	van: { imageUrl, description, type, name, price, id: vanId },
 	vanIsAvailable,
 }: VanDetailProps) {
-	const srcSet = createSrcSet(VAN_DETAIL_IMG_SIZES, imageUrl);
+	const srcSet = createResponsiveSrcSet(
+		imageUrl,
+		VAN_DETAIL_IMG_SIZES, // mobile sizes
+		VAN_DETAIL_IMG_SIZES, // desktop sizes (same as mobile)
+		1, // mobile aspect ratio (1:1 = square)
+		1, // desktop aspect ratio (1:1 = square)
+	);
 	return (
 		<div className="@container/card-full contain-content">
-			<Card className="grid @min-xl/card-full:grid-cols-2 @max-xl/card-full:grid-rows-[4fr_min-content_min-content_min-content_min-content_min-content] @min-xl/card-full:grid-rows-[.75fr_1fr] @max-xl/card-full:gap-4 gap-2 @min-xl/card-full:gap-x-4 ">
-				<CardHeader className="@min-xl/card-full:col-span-1 @min-xl/card-full:row-span-2 row-span-1 ">
+			<Card className="grid @min-xl/card-full:grid-cols-2 @min-xl/card-full:grid-rows-[auto_auto_1fr] @max-xl/card-full:gap-4 @min-xl/card-full:gap-6 gap-2">
+				<CardHeader className="@min-xl/card-full:col-span-1 @min-xl/card-full:row-span-3 row-span-1">
 					<Image
-						className="aspect-square"
+						className="aspect-square rounded-md"
 						src={imageUrl}
 						alt={description}
 						width="600"
 						height="600"
 						srcSet={srcSet}
-						sizes=" (width > 500px) 500px, 300px"
+						sizes="(min-width: 1024px) 500px, (min-width: 768px) 400px, 300px"
 					/>
 				</CardHeader>
-				<CardContent className="@min-xl/card-full:col-start-2 @max-xl/card-full:row-span-4 @min-xl/card-full:row-span-full @max-xl/card-full:row-start-2 @min-xl/card-full:row-start-1 @min-xl/card-full:grid @min-xl/card-full:grid-cols-[min-content_max-content_1fr] @min-xl/card-full:grid-rows-subgrid gap-2 @min-xl/card-full:gap-y-4">
-					<Badge
-						className="@min-xl/card-full:row-start-1 @min-xl/card-full:m-0 @min-xl/card-full:self-end "
-						variant={type}
-						size="small"
-					>
-						{type}
-					</Badge>
-					<CardTitle className="@min-xl/card-full:row-start-1 @min-xl/card-full:self-end @min-2xl/card-full:text-xl">
-						{name}
-					</CardTitle>
-					<p className="@min-xl/card-full:row-start-1 @min-xl/card-full:self-end @min-2xl/card-full:justify-self-end @min-2xl/card-full:text-2xl">
-						{displayPrice(price)}
-					</p>
-					<CardDescription className="@min-xl/card-full:col-span-3 @max-xl/card-full:row-span-2 @min-xl/card-full:row-start-2 @min-2xl/card-full:self-start @min-2xl/card-full:text-xl">
+				<CardContent className="@min-xl/card-full:col-start-2 @max-xl/card-full:row-span-4 @min-xl/card-full:row-span-3 @max-xl/card-full:row-start-2 @min-xl/card-full:row-start-1 flex @min-xl/card-full:grid @min-xl/card-full:grid-rows-[auto_auto_1fr] flex-col @min-xl/card-full:gap-4 gap-4 @min-xl/card-full:self-center">
+					{/* First row: Name and Rent button */}
+					<div className="@min-xl/card-full:row-start-1 @min-xl/card-full:flex @min-xl/card-full:items-center @min-xl/card-full:justify-between">
+						<CardTitle className="@min-xl/card-full:m-0 @min-xl/card-full:text-xl">
+							{name}
+						</CardTitle>
+						<CustomLink
+							to={
+								vanIsAvailable
+									? href('/host/rentals/rent/:vanId', { vanId })
+									: href('/vans/:vanId', { vanId })
+							}
+							className={cn(
+								badgeVariants({
+									variant: vanIsAvailable ? type : 'UNAVAILABLE',
+								}),
+								'@max-xl/card-full:hidden @min-xl/card-full:flex-shrink-0',
+							)}
+						>
+							{vanIsAvailable ? 'Rent this van' : 'Van not available'}
+						</CustomLink>
+					</div>
+
+					{/* Second row: Badge and Price */}
+					<div className="@min-xl/card-full:row-start-2 flex items-center @min-xl/card-full:justify-between gap-4">
+						<Badge
+							className="@min-xl/card-full:m-0"
+							variant={type}
+							size="small"
+						>
+							{type}
+						</Badge>
+						<p className="@min-xl/card-full:m-0 @min-xl/card-full:text-xl">
+							{displayPrice(price)}/per day
+						</p>
+					</div>
+
+					{/* Third row: Description */}
+					<CardDescription className="@min-xl/card-full:row-start-3 @min-xl/card-full:m-0 @min-xl/card-full:text-base">
 						{description}
 					</CardDescription>
 				</CardContent>
-				<CardFooter className="@min-xl/card-full:absolute @min-xl/card-full:inset-[1.5rem_calc(50%_-_1rem)_1.5rem_calc(50%_+_.5rem)] @max-lg/card-full:row-span-1 ">
+
+				{/* Mobile/Tablet Footer - hidden on desktop */}
+				<CardFooter className="@max-lg/card-full:row-span-1 @min-xl/card-full:hidden">
 					<CustomLink
 						to={
 							vanIsAvailable
@@ -67,7 +99,7 @@ export default function VanDetail({
 						}
 						className={cn(
 							badgeVariants({ variant: vanIsAvailable ? type : 'UNAVAILABLE' }),
-							'@max-lg/card-full:w-full ',
+							'@max-lg/card-full:w-full',
 						)}
 					>
 						{vanIsAvailable ? 'Rent this van' : 'Van not available'}
