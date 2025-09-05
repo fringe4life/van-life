c0bbed59899e398b85e61c865db9eda149e530e6
Coinnich <>
2025-09-06 00:40:47 +1200

    deleted unused getSkipAmount

--- app/utils/getSkipAmount.server.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @abstract gets the amount of items to skip based on page number and amount of items per page
- * @param page pageNumber
- * @param limit how many items you want per page
- * @returns {number} the amount of items to skip
- */
-export default function getSkipAmount(page: number, limit: number): number {
-	return page === 1 ? 0 : (page - 1) * limit;
-}
