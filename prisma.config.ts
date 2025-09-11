c76b218a2bc27cdc278511ff553d7ede4ae0f001
Coinnich <>
2025-09-11 23:58:35 +1200

    feat: migrated to prisma config from package.json prisma block

---
+++ prisma.config.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+import { defineConfig } from 'prisma/config';
+
+export default defineConfig({
+	schema: 'prisma',
+	migrations: {
+		seed: 'tsx prisma/seed.ts',
+	},
+});
+
+
