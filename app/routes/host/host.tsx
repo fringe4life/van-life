9e92cdd4204e6d658e369f915c4cc7c7e61e5c70
Coinnich <>
2025-09-18 12:52:26 +1200

    fix: when updating variables to use -mx pattern had missed something in the host file. also updated host file to use padding-inline for spacing

--- app/routes/host/host.tsx
+++ app/routes/host/host.tsx
@@ -73,7 +73,7 @@ export default function Host({ loaderData }: Route.ComponentProps) {
 
 	return (
 		<PendingUI as="section">
-			<div className="-mx-[var(--padding-inline)] grid w-[var(--container-layout)] grid-cols-[1fr_fit-content] items-center justify-between bg-orange-100 px-3 py-6 sm:px-6.5 sm:py-9">
+			<div className="-mx-[var(--padding-inline)] grid w-[var(--container-layout)] grid-cols-[1fr_fit-content] items-center justify-between bg-orange-100 px-[var(--padding-inline)] py-6 sm:py-9">
 				<h2 className="col-start-1 font-bold text-2xl text-neutral-900 sm:text-3xl md:text-4xl">
 					Welcome {name ? name : 'User'}!
 				</h2>
@@ -93,7 +93,7 @@ export default function Host({ loaderData }: Route.ComponentProps) {
 					Details
 				</CustomLink>
 			</div>
-			<div className="mx-[var(--full-width)] flex w-[var(--container-layout)] items-center justify-between bg-orange-200 px-3 py-6 sm:px-6.5 sm:py-9">
+			<div className="-mx-[var(--padding-inline)] flex w-[var(--container-layout)] items-center justify-between bg-orange-200 px-[var(--padding-inline)] py-6 sm:py-9">
 				<div className="font-bold text-lg text-shadow-text sm:text-2xl">
 					{typeof avgRating === 'number' ? (
 						<span>
