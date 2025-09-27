974f99cc9db300285401b8e32d4843fe6db73383
Coinnich <>
2025-09-27 12:53:52 +1200

    refactor: updated css for home page.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -43,7 +43,7 @@ export default function Home() {
 	return (
 		<PendingUI
 			as="section"
-			className="-mx-[var(--padding-inline)] relative grid aspect-[1/1.5] place-content-center self-center text-white contain-strict sm:pl-6 md:aspect-video"
+			className="-mx-[var(--padding-inline)] relative grid aspect-[1/1.5] text-white contain-strict sm:pl-6 md:aspect-video md:place-content-center md:self-center"
 		>
 			{/* Background Image with gradient overlay */}
 			<div className="mask-cover mask-no-repeat mask-right md:mask-[url(/app/assets/rvMask.svg)] absolute inset-0">
@@ -64,11 +64,11 @@ export default function Home() {
 			</div>
 
 			{/* Content overlay */}
-			<div className="relative z-20 grid content-center justify-center gap-6 px-2">
-				<h2 className="max-w-[20ch] p-0.5 font-extrabold text-2xl text-shadow-lg xs:text-3xl backdrop-blur-sm md:text-4xl">
+			<div className="z-20 grid content-center gap-y-6 px-[var(--padding-inline)] md:justify-center md:px-0">
+				<h2 className="max-w-[20ch] p-1 font-extrabold text-2xl text-shadow-lg xs:text-3xl backdrop-blur-sm md:text-4xl">
 					You got the travel plans, we got the travel vans.
 				</h2>
-				<p className="max-w-[34ch] xs:max-w-[42.5ch] p-0.5 text-shadow-sm backdrop-blur-xs">
+				<p className="max-w-[34ch] xs:max-w-[42.5ch] p-1 text-shadow-sm backdrop-blur-xs">
 					Add adventure to your life by joining the #vanlife movement. Rent the
 					perfect van to make your perfect road trip.
 				</p>
